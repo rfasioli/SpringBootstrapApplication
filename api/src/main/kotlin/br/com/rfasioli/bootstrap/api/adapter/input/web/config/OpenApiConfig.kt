9f8a95499b574c383bcc7a0d153a4a9e75b61278
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2023-10-21 16:34:04 -0300

    ajustando dependencias e configurações

--- api/src/main/kotlin/br/com/rfasioli/bootstrap/api/adapter/input/web/config/OpenApiConfig.kt
+++ api/src/main/kotlin/br/com/rfasioli/bootstrap/api/adapter/input/web/config/OpenApiConfig.kt
@@ -1,18 +1,13 @@
 package br.com.rfasioli.bootstrap.api.adapter.input.web.config
 
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig {
-    @Bean
-    fun customOpenAPI(): OpenAPI {
-        return OpenAPI().info(
-            Info().title("SDA Bootstrap App")
-                .description("This API provides resources for SDA Bootstrap Application.")
-        )
-    }
-}
+@OpenAPIDefinition(info = Info(
+    title = "Spring Bootstrap App",
+    version = "1.0",
+    description = "This API provides resources for SDA Bootstrap Application.")
+)
+class OpenApiConfig()
