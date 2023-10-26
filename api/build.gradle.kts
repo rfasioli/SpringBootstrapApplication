e33e77ab18f38862e91b2d96af90fd2482111b3e
Rodrigo Fasioli <7225579+rfasioli@users.noreply.github.com>
2023-10-25 22:08:43 -0300

    27 configurar jacoco (#40)
    * jacoco configured
    
    * gradle deprecation

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.springframework.boot")
+    id("jacoco")
 
     kotlin("jvm")
     kotlin("plugin.spring")
@@ -61,6 +62,9 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-properties-migrator:3.1.5")
 
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.tngtech.archunit:archunit-junit5:$archunitVersion")
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
@@ -76,4 +80,68 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+/************************
+ * JaCoCo Configuration *
+ ************************/
+jacoco {
+    toolVersion = "0.8.9"
+}
+
+val exclusions = listOf(
+    "**/config/**",
+    "**/application/exception/**",
+    "**/application/SdaBootstrapApplication.kt",
+    "**/domain/port/**",
+    "**/adapter/input/web/resources/**",
+    "**/adapter/input/web/advice/**",
+    "**/adapter/input/web/springdoc/**",
+    "**/adapter/output/web/persistence/entity/**",
+    "**/adapter/output/web/persistence/repository/**"
+)
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+    configure<JacocoTaskExtension> {
+        excludes = exclusions
+    }
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.jacocoTestReport)
+    violationRules {
+        rule {
+            element = "BUNDLE"
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.6".toBigDecimal()
+            }
+        }
+
+        rule {
+            element = "CLASS"
+            includes = listOf("org.gradle.*")
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "0.8".toBigDecimal()
+            }
+        }
+    }
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            exclude(exclusions)
+        }
+    )
 }
